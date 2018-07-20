228c20139d6ca8f3808dbf3ca6844637e4ca7dd3
Varun Upadhyay <varun.u28@gmail.com>
2018-07-19 22:10:27 -0700

    Added code for login and register forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '3b4718962532837b86cefb030aa93326'
 
 posts = [
 	{
@@ -26,6 +29,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+			flash(f'You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash(f'Login unsuccessful. Please check credentials', 'danger')
+	return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
